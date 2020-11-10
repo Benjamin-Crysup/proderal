563bb1bf95148382f91565a53a5f79393710b0ad
Benjamin-Crysup <57463666+Benjamin-Crysup@users.noreply.github.com>
2020-11-10 12:45:35 -0600

    Depress the turbo button.

--- Buildit_x64_linux.sh
+++ Buildit_x64_linux.sh
@@ -8,10 +8,10 @@ mkdir builds/obj_x64_linux
 mkdir builds/inc_x64_linux
 
 cp includes_com/*.h builds/inc_x64_linux/
-for v in source_com/*.cpp; do g++ -pthread $v -g -O0 -Wall -Ibuilds/inc_x64_linux -c -o builds/obj_x64_linux/$(basename $v .cpp).o; done;
-for v in source_com_any_linux/*.cpp; do g++ -pthread $v -g -O0 -Wall -Ibuilds/inc_x64_linux -c -o builds/obj_x64_linux/$(basename $v .cpp).o; done;
-for v in source_com_x64_any/*.cpp; do g++ -pthread $v -g -O0 -Wall -Ibuilds/inc_x64_linux -c -o builds/obj_x64_linux/$(basename $v .cpp).o; done;
-for v in source_proderal/*.cpp; do g++ -pthread $v -g -O0 -Wall -Ibuilds/inc_x64_linux -c -o builds/obj_x64_linux/$(basename $v .cpp).o; done;
+for v in source_com/*.cpp; do g++ -pthread $v -g -O2 -Wall -Ibuilds/inc_x64_linux -c -o builds/obj_x64_linux/$(basename $v .cpp).o; done;
+for v in source_com_any_linux/*.cpp; do g++ -pthread $v -g -O2 -Wall -Ibuilds/inc_x64_linux -c -o builds/obj_x64_linux/$(basename $v .cpp).o; done;
+for v in source_com_x64_any/*.cpp; do g++ -pthread $v -g -O2 -Wall -Ibuilds/inc_x64_linux -c -o builds/obj_x64_linux/$(basename $v .cpp).o; done;
+for v in source_proderal/*.cpp; do g++ -pthread $v -g -O2 -Wall -Ibuilds/inc_x64_linux -c -o builds/obj_x64_linux/$(basename $v .cpp).o; done;
 g++ -pthread -o builds/bin_x64_linux/proderal builds/obj_x64_linux/*.o -lz -ldl
 
 ./builds/bin_x64_linux/proderal --helpdumpgui | python3 utilities/ArgGuiBuilder.py > builds/bin_x64_linux/proderalgui.py
--- Buildit_x64_win32.bat
+++ Buildit_x64_win32.bat
@@ -8,10 +8,10 @@ MKDIR builds\obj_x64_win32
 MKDIR builds\inc_x64_win32
 
 COPY includes_com\*.h builds\inc_x64_win32
-FOR %%v in (source_com\*.cpp) DO g++ -mthreads %%v -g -O0 -Wall -Ibuilds\inc_x64_win32 -c -o builds\obj_x64_win32\%%~nv.o
-FOR %%v in (source_com_any_win32\*.cpp) DO g++ -mthreads %%v -g -O0 -Wall -Ibuilds\inc_x64_win32 -c -o builds\obj_x64_win32\%%~nv.o
-FOR %%v in (source_com_x64_any\*.cpp) DO g++ -mthreads %%v -g -O0 -Wall -Ibuilds\inc_x64_win32 -c -o builds\obj_x64_win32\%%~nv.o
-FOR %%v in (source_proderal\*.cpp) DO g++ -mthreads %%v -g -O0 -Wall -Ibuilds\inc_x64_win32 -c -o builds\obj_x64_win32\%%~nv.o
+FOR %%v in (source_com\*.cpp) DO g++ -mthreads %%v -g -O2 -Wall -Ibuilds\inc_x64_win32 -c -o builds\obj_x64_win32\%%~nv.o
+FOR %%v in (source_com_any_win32\*.cpp) DO g++ -mthreads %%v -g -O2 -Wall -Ibuilds\inc_x64_win32 -c -o builds\obj_x64_win32\%%~nv.o
+FOR %%v in (source_com_x64_any\*.cpp) DO g++ -mthreads %%v -g -O2 -Wall -Ibuilds\inc_x64_win32 -c -o builds\obj_x64_win32\%%~nv.o
+FOR %%v in (source_proderal\*.cpp) DO g++ -mthreads %%v -g -O2 -Wall -Ibuilds\inc_x64_win32 -c -o builds\obj_x64_win32\%%~nv.o
 g++ -mthreads -o builds\bin_x64_win32\proderal.exe builds\obj_x64_win32\*.o -lz -static-libgcc -static-libstdc++ -static -lpthread
 
 builds\bin_x64_win32\proderal --helpdumpgui | python utilities\ArgGuiBuilder.py > builds\bin_x64_win32\proderalgui.py
